0d5a081976ce2198d6df81ad019da89a7237516e
Michael Howard <michael.howard.h@gmail.com>
2020-06-06 12:58:37 +0200

    Init templates

---
+++ java-solution/src/test/java/com/thiefspin/javasolution/JavaSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiefspin.javasolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
